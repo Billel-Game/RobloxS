f64d87b8498a384905ec4069e97222276ca85d98
Billel <85782585+Billel-Game@users.noreply.github.com>
2025-08-11 04:19:58 +0200

    a
    a

--- my fishing.lua
+++ my fishing.lua
@@ -1,9 +1,9 @@
-local omni = loadstring(game:HttpGet("https://raw.githubusercontent.com/Billel-Game/RobloxS/refs/heads/main/uilib.lua"))()
+local omni = loadstring(game:HttpGet("https://raw.githubusercontent.com/Billel-Game/RobloxS/refs/heads/main/libtest.lua"))()
 omni:ShowKeyPrompt(
     "https://raw.githubusercontent.com/Billel-Game/RobloxS/main/broken",
     "https://link-target.net/1380127/e5ro3DcEbUkf"
 )
-
+--// UI Setup
 local UI = omni.new({
     Name = "🔥 Fury Scripts 🔥",
     Credit = "Created by Billel",
@@ -11,7 +11,29 @@ local UI = omni.new({
     Discord = "Billel"
 })
 
-omni:CreateToggleButton(UI, 133641333781908)
+local player = game.Players.LocalPlayer
+local playerGui = player:WaitForChild("PlayerGui")
+
+local screenGui = Instance.new("ScreenGui")
+screenGui.Name = "FuryScreenGui"
+screenGui.ResetOnSpawn = false
+screenGui.Parent = playerGui
+
+local toggleButton = Instance.new("ImageButton")
+toggleButton.Name = "FuryUIToggle"
+toggleButton.Image = "rbxassetid://133641333781908"
+toggleButton.Size = UDim2.new(0, 50, 0, 50)
+toggleButton.Position = UDim2.new(0, 10, 0, 10)
+toggleButton.BackgroundColor3 = Color3.fromRGB(122, 28, 187)
+toggleButton.BackgroundTransparency = 0.2
+toggleButton.ZIndex = 999
+toggleButton.BorderSizePixel = 3
+toggleButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+toggleButton.Parent = screenGui
+
+toggleButton.MouseButton1Click:Connect(function()
+    UI.container.Main.Visible = not UI.container.Main.Visible
+end)
 
 local mainPage = UI:CreatePage("Main 🏠")
 local mainSection = mainPage:CreateSection("Main (Toggles)")
